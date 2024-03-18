1312a1b7943e07f1799acc5470ade755a4d61e84
2023450 <2023450@B001-15>
2024-03-18 18:00:19 +0100

    nav_hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const hero = () => {
   return (
-    <div>hero</div>
+    <section id='hero'>
+      <div className="wrapper">
+        <div>
+          <h1>Next generation digital banking</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aliquid, inventore quisquam animi aliquam voluptatem nisi.</p>
+            <button>Request invite</button>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 const navbar = () => {
   return (
     <nav>
-      <span className="logo">easybank</span>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="x">Carers</a></li>
-      </ul>
+      <div className='wrapper'>
+        <span className="logo">easybank</span>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="x">Carers</a></li>
+        </ul>
 
-      <button>Request invite</button>
+        <button>Request invite</button>
+      </div>
+      
     </nav>
   )
 }
