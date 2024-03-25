1cc1844b2af32f7c3e4284505261f886921c4e3f
2023450 <2023450@B001-15>
2024-03-25 18:39:58 +0100

    0325_latest elkészült

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const latest = () => {
+  const [apidata, setApiData] = useState([])
+  useEffect(() =>{
+    fetch("http://localhost:8000/latest.json")
+    .then(res => res.json())
+    .then(data => setApiData(data))
+  }, [])
+
+
+
   return (
-    <div>latest</div>
+    <section id="latest">
+      <div className='wrapper'>
+        <h2>Latest</h2>
+        <div className='cards'>
+
+          {apidata.map((data,idx) => <LatestCard key={idx} {...data} />) }
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
+const LatestCard = (props) => {
+  return (
+    <div className='latest-card'>
+      <img src={"http://localhost:8000/" + props.img} alt="{props.title}" />
+      <div>
+        <h6>By {props.creator}</h6>
+        <h3>{props.title}</h3>
+        <p>{props.content}</p>
+      </div>
+      
+    </div>
   )
 }
 
